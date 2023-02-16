459307e759f339abc4a97fa5028720e35df4b815
Pranav Hari Krishna <121807966+pranav-seven@users.noreply.github.com>
2023-02-17 01:21:41 +0530

    Delete dto directory

--- dto/Admin.java
+++
@@ -1,8 +0,0 @@
-package com.conapp.dto;
-
-public class Admin extends User{
-    public Admin(String name, String username, String password)
-    {
-        super(name, username, password);
-    }
-}
