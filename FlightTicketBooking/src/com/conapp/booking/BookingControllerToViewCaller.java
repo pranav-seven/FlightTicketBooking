acb6f882f58207ec1b7a27b714cc94c45b2ca7a6
Pranav Hari Krishna <121807966+pranav-seven@users.noreply.github.com>
2023-02-17 16:14:48 +0530

    Update BookingControllerToViewCaller.java

--- FlightTicketBooking/src/com/conapp/booking/BookingControllerToViewCaller.java
+++ FlightTicketBooking/src/com/conapp/booking/BookingControllerToViewCaller.java
@@ -2,4 +2,5 @@ package com.conapp.booking;
  import com.conapp.dto.User;
 public interface BookingControllerToViewCaller {
     public User getUser();
+    public void printMessage(String message);
 }
