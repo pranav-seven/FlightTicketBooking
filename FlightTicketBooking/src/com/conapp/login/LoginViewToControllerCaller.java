d0e70f3150e5ffb68d9b4795ce27ea080bc21645
Pranav Hari Krishna <121807966+pranav-seven@users.noreply.github.com>
2023-02-20 19:43:47 +0530

    Update LoginViewToControllerCaller.java

--- FlightTicketBooking/src/com/conapp/login/LoginViewToControllerCaller.java
+++ FlightTicketBooking/src/com/conapp/login/LoginViewToControllerCaller.java
@@ -4,4 +4,5 @@ public interface LoginViewToControllerCaller {
     void checkLoginDetailsOnline(String username, String password);
     boolean checkUsername(String username);
     void addUser(String name, String username, String password);
+    boolean checkNewPassword(String password, String rePassword);
 }
